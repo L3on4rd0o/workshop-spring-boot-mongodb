8eeac6287d643bab25babf5f366b0bc31c74b480
Leonardo Alvarenga <leonardoalvarengacordeiro@gmail.com>
2023-01-31 20:37:20 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/leoalvarenga/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leoalvarenga.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
