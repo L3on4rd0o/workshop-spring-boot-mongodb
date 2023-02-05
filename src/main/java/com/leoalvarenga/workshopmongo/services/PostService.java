eb38b15874c8d6467e9e778631a0faf474e9707c
Leonardo Alvarenga <leonardoalvarengacordeiro@gmail.com>
2023-02-05 14:56:31 -0300

    Obtendo um post por id

---
+++ src/main/java/com/leoalvarenga/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.leoalvarenga.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leoalvarenga.workshopmongo.domain.Post;
+import com.leoalvarenga.workshopmongo.repository.PostRepository;
+import com.leoalvarenga.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
